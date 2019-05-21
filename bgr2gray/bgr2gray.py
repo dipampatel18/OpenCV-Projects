677cea6d9d7adb53521f6d90c019f0fa15bddf92
Dipam Patel <dipu18@terpmail.umd.edu>
2019-05-21 14:34:48 -0400

    Add files via upload

---
+++ bgr2gray/bgr2gray.py
@@ -0,0 +1,26 @@
+##################################################
+# Author: Dipam Patel
+# Project: OpenCV - RGB to Gray
+# GitHub: ter.ps/dipam2
+##################################################
+
+# Importing Packages
+# import numpy as np
+import cv2
+# import matplotlib.pyplot as plt
+
+# Reading Image
+image = cv2.imread('pens.jpg')
+
+## Method-1 ##
+# Directly reading image in grayscale format
+# gray_image = cv2.imread('pens.jpg', cv2.IMREAD_GRAYSCALE)
+
+## Method-2 ##
+# Converting color image to gray
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Showing Image
+cv2.imshow('Gray Image', gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

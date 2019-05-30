19258910f014cfb07ec43c412830563127dd0fbe
Dipam Patel <dipu18@terpmail.umd.edu>
2019-05-30 10:31:59 -0400

    Update bgr2gray.py

--- bgr2gray/bgr2gray.py
+++ bgr2gray/bgr2gray.py
@@ -22,5 +22,9 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Showing Image
 cv2.imshow('Gray Image', gray_image)
+
+# Saving Image
+cv2.imwrite('Gray Image.jpg', image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
